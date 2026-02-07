8e1c77d3e7bdf618627c67f955a394e74b96aa7a
Jason <jason.howell@thetalake.com>
2026-02-07 12:11:13 -0800

    feat: add calendar, day, and month components with tests
    - add Calendar, Day, and Month components with basic templates
    - include initial SCSS files for each component
    - add unit tests for Calendar, Day, and Month components

---
+++ src/app/month/month.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-month',
+  imports: [],
+  templateUrl: './month.html',
+  styleUrl: './month.scss',
+})
+export class Month {
+
+}
